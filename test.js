6d55e24a6610c76d8e0eacd0fd041241be180022
rainyjune <rainyjune@live.cn>
2015-05-07 19:09:06 +0800

    Update test.js

--- test.js
+++ test.js
@@ -4,41 +4,45 @@ window.onload = function(){
   function log(msg) {
     document.querySelector("#debug").innerHTML = msg;
   }
+  window.log = log;
   
   var testDiv = document.getElementById("testDiv");
-  testDiv.addEventListener("swipeStart", function(e){
+  
+  var divObj = new TouchObject(testDiv);
+  
+  divObj.addEventListener("swipeStart", function(e){
     number = 0;
     log(number);
     log("swipeStart");
   });
-  testDiv.addEventListener("swipeLeft", function(e){
+  divObj.addEventListener("swipeLeft", function(e){
     console.log("swipeLeft", e);
     log("swipeLeft");
   });
-  testDiv.addEventListener("swipeRight", function(e){
+  divObj.addEventListener("swipeRight", function(e){
     //alert("sss");
      console.log("swipeRight", e);
     log("swipeRight");
   })
   
-  testDiv.addEventListener("swipeUp", function(e){
+  divObj.addEventListener("swipeUp", function(e){
     //alert("sss");
      console.log("swipeUp", e);
     log("swipeUp");
   })
-  testDiv.addEventListener("swipeDown", function(e){
+  divObj.addEventListener("swipeDown", function(e){
     //alert("sss");
      console.log("swipeDown", e);
     log("swipeDown");
   })
   
-  testDiv.addEventListener("swipeProgress", function(e) {
+  divObj.addEventListener("swipeProgress", function(e) {
     console.log("swipeProgress", e);
     number += 1;
     
-    log("" + number + "," + e.detail.movedPageX);
+    log("" + e.detail.movedPageX + "," + e.detail.movedPageY);
   });
-  testDiv.addEventListener("swipeCancel", function(e) {
+  divObj.addEventListener("swipeCancel", function(e) {
     log("swipeCancel");
   });
 };
