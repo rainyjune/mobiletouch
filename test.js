abf83372c080bc3c7f8f50b2bd307604d65a4477
rainyjune <rainyjune@live.cn>
2015-05-07 09:34:19 +0800

    Rename showNumber() to log()

--- test.js
+++ test.js
@@ -1,44 +1,44 @@
 window.onload = function(){
   var number = 0;
   
-  function showNumber(newNumber) {
-    document.querySelector("#debug").innerHTML = newNumber;
+  function log(msg) {
+    document.querySelector("#debug").innerHTML = msg;
   }
   
   var testDiv = document.getElementById("testDiv");
   testDiv.addEventListener("swipeStart", function(e){
     number = 0;
-    showNumber(number);
+    log(number);
     //alert("swipeStart");
   });
   testDiv.addEventListener("swipeLeft", function(e){
     console.log("swipeLeft", e);
-    alert("swipeLeft");
+    log("swipeLeft");
   });
   testDiv.addEventListener("swipeRight", function(e){
     //alert("sss");
      console.log("swipeRight", e);
-    alert("swipeRight");
+    log("swipeRight");
   })
   
   testDiv.addEventListener("swipeUp", function(e){
     //alert("sss");
      console.log("swipeUp", e);
-    alert("swipeUp");
+    log("swipeUp");
   })
   testDiv.addEventListener("swipeDown", function(e){
     //alert("sss");
      console.log("swipeDown", e);
-    alert("swipeDown");
+    log("swipeDown");
   })
   
   testDiv.addEventListener("swipeProgress", function(e) {
     console.log("swipeProgress", e);
     number += 1;
     
-    showNumber("" + number + "," + e.detail.movedPageX);
+    log("" + number + "," + e.detail.movedPageX);
   });
   testDiv.addEventListener("swipeCancel", function(e) {
-    alert("swipeCancel");
+    log("swipeCancel");
   });
 };
