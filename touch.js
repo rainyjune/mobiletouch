b9d7ffdf696b8c50914b338b5da96deadb073f4d
rainyjune <rainyjune@live.cn>
2015-05-08 17:11:47 +0800

    Update touch.js: Add Function.prototype.bind Polyfill

--- touch.js
+++ touch.js
@@ -404,6 +404,34 @@
     }
   })();
   
+  // Polyfill for Function.prototype.bind
+  // This feature is not supported natively on some platforms, such as Android 2.3.5
+  // from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
+  if (!Function.prototype.bind) {
+    Function.prototype.bind = function(oThis) {
+      if (typeof this !== 'function') {
+        // closest thing possible to the ECMAScript 5
+        // internal IsCallable function
+        throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+      }
+
+      var aArgs   = Array.prototype.slice.call(arguments, 1),
+          fToBind = this,
+          fNOP    = function() {},
+          fBound  = function() {
+            return fToBind.apply(this instanceof fNOP
+                   ? this
+                   : oThis,
+                   aArgs.concat(Array.prototype.slice.call(arguments)));
+          };
+
+      fNOP.prototype = this.prototype;
+      fBound.prototype = new fNOP();
+
+      return fBound;
+    };
+  }
+  
   if (typeof define === "function" && (define.amd || define.cmd)) {
     define(function(){
       return TouchObject;
