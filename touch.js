9df5ac13c2a0747e8185b300a19331753796d269
rainyjune <rainyjune@live.cn>
2015-05-12 11:26:03 +0800

    Update touch.js: Add approximatelyEqual() function

--- touch.js
+++ touch.js
@@ -497,6 +497,10 @@
   function swipeDirection(x1, x2, y1, y2) {
     return Math.abs(x1 - x2) >= Math.abs(y1 - y2) ? (x1 - x2 > 0 ? 'Left' : 'Right') : (y1 - y2 > 0 ? 'Up' : 'Down');
   }
+
+  function approximatelyEqual(x, y) {
+    return Math.abs(x - y) < 2;
+  }
   
   function ongoingTouchIndexById(idToFind, touches) {
     for (var i=0; i < touches.length; i++) {
