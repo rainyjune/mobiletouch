c3921157cae3eb3c7bdbe95ada410aab7c2a8ce5
rainyjune <rainyjune@live.cn>
2015-05-15 17:22:17 +0800

    Update hello.html

--- playground/hello.js
+++ playground/hello.js
@@ -13,8 +13,28 @@ function log(touchEvent) {
   $("#touches").innerHTML = touchEvent.touches.length;
   $("#targetTouches").innerHTML = touchEvent.targetTouches.length;
   $("#changedTouches").innerHTML = touchEvent.changedTouches.length;
+
+  if (touchEvent.touches.length) {
+    var str = generateTouchEventDom(touchEvent.touches[0]).innerHTML;
+    $("#touchesInfo").innerHTML = str;
+  } else {
+    $("#touchesInfo").innerHTML = "none";
+  }
 }
 
 function $(selector) {
   return document.querySelector(selector);
+}
+
+function generateTouchEventDom(touchEvent) {
+  var dom = $("#touch-template").cloneNode(true);
+  dom.querySelector(".identifier").innerHTML = touchEvent.identifier;
+  dom.querySelector(".target").innerHTML = touchEvent.target.tagName;
+  dom.querySelector(".screenX").innerHTML = touchEvent.screenX;
+  dom.querySelector(".screenY").innerHTML = touchEvent.screenY;
+  dom.querySelector(".clientX").innerHTML = touchEvent.clientX;
+  dom.querySelector(".clientY").innerHTML = touchEvent.clientY;
+  dom.querySelector(".pageX").innerHTML = touchEvent.pageX;
+  dom.querySelector(".pageY").innerHTML = touchEvent.pageY;
+  return dom;
 }
